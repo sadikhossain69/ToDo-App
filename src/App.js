279b95939f3444eaa70bfa44a3873b692c563c3f
sadikhossain69 <sadikhossain433@gmail.com>
2022-06-30 23:57:25 +0600

    register form design

--- src/App.js
+++ src/App.js
@@ -4,16 +4,18 @@ import { Routes, Route } from 'react-router-dom'
 import Login from './components/Login';
 import ToDo from './components/ToDo';
 import Footer from './components/Footer';
+import Register from './components/Register';
 
 function App() {
   return (
     <section>
       <Navbar />
       <Routes>
-        <Route path='/' element={<ToDo/>} />
-        <Route path='/login' element={<Login/>} />
+        <Route path='/' element={<ToDo />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/register' element={<Register />} />
       </Routes>
-      <Footer/>
+      <Footer />
     </section>
   );
 }
--- src/components/Register.js
+++ src/components/Register.js
@@ -1,9 +1,35 @@
 import React from 'react';
+import { Link } from 'react-router-dom'
 
 const Register = () => {
     return (
-        <section>
-            <h2 className="text-3xl">This is Register!!!</h2>
+        <section className='min-h-screen flex justify-center items-center'>
+            <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+                <h2 className="text-3xl font-bold text-center mt-5 text-green-600">Register</h2>
+                <div className="card-body">
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="label-text">Email</span>
+                        </label>
+                        <input type="text" placeholder="email" className="input input-bordered" />
+                    </div>
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="label-text">Password</span>
+                        </label>
+                        <input type="text" placeholder="password" className="input input-bordered" />
+                        <label className="label">
+                            <p className="label-text-alt">Already have an account? <Link className='text-green-500' to='/login' >Login</Link></p>
+                        </label>
+                    </div>
+                    <div className="form-control mt-6">
+                        <button className="btn btn-primary">Login</button>
+                    </div>
+                    <div className="form-control">
+                        <button className="btn btn-primary border-0 hover:bg-green-500 bg-green-600">Continue With Google</button>
+                    </div>
+                </div>
+            </div>
         </section>
     );
 };
