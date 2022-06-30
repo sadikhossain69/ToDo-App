5dc7c1a19a5c132aad126569a557f085acf06919
sadikhossain69 <sadikhossain433@gmail.com>
2022-06-30 22:07:59 +0600

    daisy ui installed

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div>
-      <h1 className="text-3xl font-bold underline text-red-600">
+      <h1 className="text-3xl font-bold underline">
         Hello world!
       </h1>
     </div>
